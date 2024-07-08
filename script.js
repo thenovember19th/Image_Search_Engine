6a5d0966ef92af9566322d613536bd3bf98c235d
minrajbhatta.com <bhattaminraj88@gmail.com>
2024-07-08 13:40:04 +0545

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
+accessKey = CnlRuNPRQO_JV9XtItY2IDLp72QaVkgaDkJBJCiMuPw;
+
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
 const showMoreBtn = document.getElementById("show-more-btn");
 
 let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
+
+    const response = await fetch(url)
+    const data = await response.json();
+   
+    const results = data.results;
+    results.map((result) =>{
+        const image = document.createElement("img");
+        image_src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank"; 
+        imageLink.appendChild(Image);
+        searchResult.appendChild(imageLink);
+    })
+}
+
+
+searchForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    page = 1;
+    searchImages()
+});
 
