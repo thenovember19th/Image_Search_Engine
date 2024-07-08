a63f3484560302606c8a941f2797f06400860f4f
minrajbhatta.com <bhattaminraj88@gmail.com>
2024-07-08 13:23:51 +0545

    first init

---
+++ script.js
@@ -0,0 +1,7 @@
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+
